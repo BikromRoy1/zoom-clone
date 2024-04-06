db3df62605610c1cc8bc7c3f80b14f26c402fb14
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-04-06 15:58:16 +0600

    meeting add

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import { ReactNode } from 'react';
 
-const MeetingModal = () => {
-    return <div>MeetingModal</div>;
+import { Dialog, DialogContent } from '@/components/ui/dialog';
+
+interface MeetingModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  className?: string;
+  children?: ReactNode;
+  handleClick?: () => void;
+  buttonText?: string;
+  image?: string;
+  buttonIcon?: string;
+}
+
+const MeetingModal = ({
+  isOpen,
+  onClose,
+  children,
+  title,
+  className,
+  buttonText,
+  handleClick,
+  image,
+  buttonIcon,
+}: MeetingModalProps) => {
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className='flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1'></DialogContent>
+    </Dialog>
+  );
 };
 
 export default MeetingModal;
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -10,6 +10,8 @@ const MeetingTypeList = () => {
   const [meetingState, setMeetingState] = useState<
     'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
   >();
+
+  const createMeeting = () => {};
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
       <HomeCard
@@ -39,7 +41,14 @@ const MeetingTypeList = () => {
         className='bg-yellow-1'
         handleClick={() => router.push('/recordings')}
       />
-      <MeetingModal />
+      <MeetingModal
+        isOpen={meetingState === 'isInstantMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title='Start an Instant Meeting'
+        className='text-center'
+        buttonText='Start Meeting'
+        handleClick={createMeeting}
+      />
     </section>
   );
 };
